2e4cc1664d2f6c5fa745e5631a986fcee4eb4cba
jjesu <jjesu@LAPTOP-40F3KJGL>
2021-02-12 21:46:21 -0600

    HR VER 2

--- src/humanresorces/Humanresorces.java
+++ src/humanresorces/Humanresorces.java
@@ -5,9 +5,11 @@
  */
 package humanresorces;
 
+import mx.edu.cesba.model.Employee;
+
 /**
  *
- * @author jjesu
+ * @author jjesus
  */
 public class Humanresorces {
 
@@ -16,6 +18,35 @@ public class Humanresorces {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+         //creacion de objetos
+        Employee e1 = new Employee (); // sintasix para crear un objeto en java
+        Employee e2 = new Employee ();
+        
+        e1.id = 1;
+        e1.firstName = "tiburcio";
+        e1.lastName = "Smith";
+        e1.age =  20;
+        e1.email = "tiburcio.cesba.edu.mx";
+        e1.phoneNumber = 123456709;
+        e1.salary = 30000.25;
+        e1.hiredate = "02-02-21";
+        
+        
+        e2.id = 2;
+        e2.firstName = ("Felipa");
+        e2.lastName = "Perez";
+        e2.age = 30;
+        e2.email = "felipa.cesba.edu.mx";
+        e2.phoneNumber = 256478987;
+        e2.salary = 33000.33;
+        e2.hiredate = "01-01-21";
+        
+        e1.getDetails();
+        System.out.println("------------------------");
+        e2.getDetails();
+        
+        
     }
     
 }
